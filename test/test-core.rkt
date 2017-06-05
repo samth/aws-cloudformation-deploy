6c09c018f7dc350a94f6b16697c6fff11e688101
Michael Arnold <marnold@cj.com>
2017-06-05 10:55:27 -0700

    CJPM-5933: name tests

--- test/test-core.rkt
+++ test/test-core.rkt
@@ -3,25 +3,37 @@
 (require rackunit
          "../src/core.rkt")
 
-(check-equal? (make-aws-cli-filepath "/testpath/test-template.yaml")
-              "file:///testpath/test-template.yaml")
+(test-case
+ "make-aws-cli-filepath"
+ (check-equal? (make-aws-cli-filepath "/testpath/test-template.yaml")
+              "file:///testpath/test-template.yaml"))
 
-(check-equal? (make-tags
+(test-case
+ "make-tags"
+ (check-equal? (make-tags
                #:application "test-app"
                #:squad "test-squad"
                #:environment "test-env")
-              (list "--tags" "Key=cj,Value=application=test-app" "Key=cj,Value=squad=test-squad" "Key=cj,Value=environment=test-env"))
+              (list "--tags" "Key=cj,Value=application=test-app" "Key=cj,Value=squad=test-squad" "Key=cj,Value=environment=test-env")))
 
-(check-equal? (make-aws-parameters (list (cons "test-key" "test-val")))
-              (list "--parameters" "ParameterKey=test-key,ParameterValue=test-val"))
+(test-case
+ "make-aws-parameters single param"
+ (check-equal? (make-aws-parameters (list (cons "test-key" "test-val")))
+              (list "--parameters" "ParameterKey=test-key,ParameterValue=test-val")))
 
-(check-equal? (make-aws-parameters (list (cons "test-key" "test-val") (cons "test-key-1" "test-val-1")))
-              (list "--parameters" "ParameterKey=test-key,ParameterValue=test-val" "ParameterKey=test-key-1,ParameterValue=test-val-1"))
+(test-case
+ "make-aws-parameters multi-param"
+ (check-equal? (make-aws-parameters (list (cons "test-key" "test-val") (cons "test-key-1" "test-val-1")))
+              (list "--parameters" "ParameterKey=test-key,ParameterValue=test-val" "ParameterKey=test-key-1,ParameterValue=test-val-1")))
 
-(check-equal? (make-stack-policy-path "/Users/test-user/project/test-policy.json")
-              (list "--stack-policy-body" "file:///Users/test-user/project/test-policy.json"))
+(test-case
+ "make-stack-policy-path"
+ (check-equal? (make-stack-policy-path "/Users/test-user/project/test-policy.json")
+              (list "--stack-policy-body" "file:///Users/test-user/project/test-policy.json")))
 
-(check-equal? (make-aws-cli-arguments "create-stack"
+(test-case
+ "make-aws-cli-arguments"
+ (check-equal? (make-aws-cli-arguments "create-stack"
                                       "test-stack"
                                       "/test-path/to-template.yaml"
                                       "test-app"
@@ -35,5 +47,5 @@
                     "--capabilities" "CAPABILITY_IAM"
                     "--tags" "Key=cj,Value=application=test-app" "Key=cj,Value=squad=test-squad" "Key=cj,Value=environment=test-env"
                     "--parameters" "ParameterKey=test-key,ParameterValue=test-val" "ParameterKey=test-key2,ParameterValue=test-val2"
-                    "--stack-policy-body" "file:///test-path/to-stack-policy.json"))
+                    "--stack-policy-body" "file:///test-path/to-stack-policy.json")))
 
